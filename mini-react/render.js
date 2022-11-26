d935a668d15972cf98896b929ee30e4c6938b1a8
Han Wang <hanwangx@gmail.com>
2022-11-26 17:56:41 +0800

    doc: add some comments on render api

--- mini-react/render.js
+++ mini-react/render.js
@@ -1,15 +1,20 @@
 function render(element, container) {
+    //create parent node
   const dom =
     element.type === "TEXT_ELEMENT"
       ? document.createTextNode(element.props.nodeValue)
       : document.createElement(element.type);
 
+// append props to the parent node(exclude children key in props)
   Object.keys(element.props)
     .filter((key) => key !== "children")
     .forEach((key) => (dom[key] = element.props[key]));
 
+// recursivly run though the children to create the children node 
+
   element.props.children.forEach((child) => render(child, dom));
 
+  // append children node to its parents
   container.append(dom);
 }
 
